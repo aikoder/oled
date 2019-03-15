90c68666040e58bdccfa11e40a0a82b5b49ddb8e
Gavin <gavin.huang@aztech.com>
2019-03-15 13:30:02 +0800

    add new driver

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# oled
+# SSD1306 and SH1106 driver
 oled samples
---
+++ ssd1306/configs.h
@@ -0,0 +1,8 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+// Needed includes
+#include "stm32f1xx_hal.h"
+#include "i2c.h"
+
+#endif // _CONFIG_H
